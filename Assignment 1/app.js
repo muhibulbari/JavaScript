18608165482fd5c04518f3fc12d994e903973505
Muhib ul Bari <128531500+muhibulbari@users.noreply.github.com>
2023-04-07 02:34:12 +0500

    app.js

---
+++ Assignment 1/app.js
@@ -0,0 +1,85 @@
+// Q.1)Write a JavaScript program that accepts two numbers in two prompts and
+// displays the larger one in the console.
+
+// let user1FavNum = prompt("What is your favourite number?");
+// let user2FavNum = prompt("What is your favourite number?");
+// console.log(Math.max(user1FavNum, user2FavNum));
+
+// Q.2)Write a JavaScript conditional statement to find the sign of a number. Display
+// an alert box with the specified sign.
+
+// let userNum = prompt("Write any positive or negative number");
+// if (userNum >= 0) {
+//   console.log("The sign is +" + userNum);
+// } else if (userNum < 0) {
+//   console.log("The sign is -" + (-userNum));
+// }
+
+// Q.3)Write a JavaScript program that accepts five numbers in five prompts and
+// displays the larger one in the console.
+
+// let user1FavNum = prompt("What is your favourite number?");
+// let user2FavNum = prompt("What is your favourite number?");
+// let user3FavNum = prompt("What is your favourite number?");
+// let user4FavNum = prompt("What is your favourite number?");
+// let user5FavNum = prompt("What is your favourite number?");
+// console.log(Math.max(user1FavNum, user2FavNum, user3FavNum, user4FavNum, user5FavNum));
+
+// Q.4)Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it
+// will check if the current number is odd or even, and display a message to the
+// screen.
+
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//     document.write(i + " is even ");
+//     } else {
+//     document.write(i + " is odd ");
+//     }
+// }
+
+// Q.5) Write a JavaScript program which computes the average marks of the
+// following students Then, this average is used to determine the corresponding
+// grade.
+// The grades are computed as follows :
+// Range Grade
+// <60 F <70 D <80 C <90 B <100 A
+
+// let userGrade = +prompt("Enter your Marks");
+// if (userGrade < 60) {
+//   console.log("F");
+// } else if (userGrade < 70) {
+//   console.log("D");
+// } else if (userGrade < 80) {
+//   console.log("C");
+// } else if (userGrade < 90) {
+//   console.log("B");
+// } else {
+//   console.log("A");
+// }
+
+// Q.6)Write a JavaScript program which iterates the integers from 1 to 100. But for
+// multiples of three print "Fizz" instead of the number and for the multiples of five
+// print "Buzz". For numbers which are multiples of both three and five print
+// "FizzBuzz".
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Q.7)Write a JavaScript program to construct the following pattern, using a nested
+// for loop.
+// *
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
+
+//document.write(" * <br> ** <br> *** <br> **** <br> ***** ");
